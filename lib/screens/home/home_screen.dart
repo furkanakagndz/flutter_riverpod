548673facd8e0f4a0fbb66e812add12cd48637fd
furkan <fakagunduz98@gmail.com>
2024-10-30 01:18:29 +0300

    updating state

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -38,10 +38,14 @@ class HomeScreen extends ConsumerWidget {
                   Text('£${allProducts[index].price}'),
 
                   if (cartProducts.contains(allProducts[index]))
-                    TextButton(onPressed: () {}, child: const Text('Remove')),
+                    TextButton(onPressed: () {
+                      ref.read(cartNotifierProvider.notifier).removeProduct(allProducts[index]);
+                    }, child: const Text('Remove')),
 
                   if (!cartProducts.contains(allProducts[index]))
-                    TextButton(onPressed: () {}, child: const Text('Add to Cart')),
+                    TextButton(onPressed: () {
+                      ref.read(cartNotifierProvider.notifier).addProduct(allProducts[index]);
+                    }, child: const Text('Add to Cart')),
                 ],
               ),
             );
